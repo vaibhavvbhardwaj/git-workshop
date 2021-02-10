abd04cb0b88b7044ccc44230d963aa52b4e38f00
Vaibhav Bhardwaj <vaibhavb@leewayhertz.com>
2021-02-10 11:44:09 +0530

    new volume formula added

--- index2.js
+++ index2.js
@@ -1,9 +1,11 @@
 const square = {
     'area': (a) => (a*a),
-    'perimeter' : (a) => (4*a)
+    'perimeter' : (a) => (4*a),
+    'volume' : (a) => (a*a*a)
 }
 const calsquare = (a) => {
     console.log('The area of the square is ' + square.area(a) );
     console.log('The perimeter of the square is ' + square.perimeter(a) );
+    console.log('The volume of the cube is' + square.volume(a) );
 }
 calsquare(5);
