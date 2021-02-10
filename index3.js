cc6111669719b14add0f62a6f96e830baebb967e
Vaibhav Bhardwaj <vaibhavb@leewayhertz.com>
2021-02-10 09:57:23 +0530

    path and extension of file

--- index3.js
+++ index3.js
@@ -1,1 +1,11 @@
 console.log("This is the new code for development branch")
+
+const path = require('path');
+
+const filename = path.join(__filename);
+const basename = path.basename(filename);
+const extname = path.extname('basename');
+
+console.log(filename);
+console.log(basename);
+console.log(extname);
